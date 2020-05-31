6c6309a0d2a55928977cb9a743b02814216df1b8
ishibashi.futos <ishibashi.futos@outlook.com>
2020-05-31 15:14:02 +0900

    Java11でもビルドできるようにしてみた

--- src/main/java/com/github/fishibashi/App.java
+++ src/main/java/com/github/fishibashi/App.java
@@ -4,7 +4,8 @@ import org.apache.commons.lang3.SystemUtils;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello, Ant");
+    String hello = "Hello, Ant.";
+    System.out.println(hello);
     System.out.println("* Java version : " + SystemUtils.JAVA_VM_VERSION);
     System.out.println("* OS name :" + SystemUtils.OS_NAME);
     System.out.println("* OS version :" + SystemUtils.OS_VERSION);
